6ca175a5aa8c20ca3d35b2fb75696637d51825b0
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-13 12:13:02 -0800

    Update program.cpp

--- code/program.cpp
+++ code/program.cpp
@@ -4,8 +4,11 @@
 // gmail: <michaelbrockus@gmail.com>
 //
 #include "program.hpp"
-#include <iostream>
+#include <cstdio> 
 #include <cstdlib>
+#include <cstring>
+#include <cerrno>
+#include <vector>
 
 //
 // Greet the user
@@ -20,8 +23,47 @@ const char * greet(void)
 // application logic must be implemented
 // in the foundation.
 //
-int foundation(void)
+int foundation(int argc, char **argv)
 {
-    std::cout << greet() << std::endl;
+    std::vector<FILE*> files;
+    if(argc < 2)
+    {
+        files.push_back(stdin);
+    } // end if
+    else
+    {
+        for(int i = 1; i < argc; i++)
+        {
+            if(strcmp(argv[i], "-") == 0)
+            {
+                files.push_back(stdin);
+                continue;
+            } // end if
+
+            FILE* f = fopen(argv[i], "r");
+            if(!f)
+            {
+                fprintf(stderr, "%s: %s: %s\n", argv[0], argv[i], strerror(errno));
+                continue;
+            } // end if
+
+            files.push_back(f);
+        } // end for
+
+    } // end else
+
+    for (FILE *&f : files)
+    {
+        while (char c = fgetc(f))
+        {
+            if (feof(f))
+            {
+                break;
+            } // end if
+
+            fputc(c, stdout);
+        } // end while
+
+    } // end for
     return EXIT_SUCCESS;
 } // end of function foundation
